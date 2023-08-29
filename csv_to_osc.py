2eb09a7465dbabc524fbd1cc28bf5cd4e1117d9a
Yau-Meng Wong <102400085+y4umeng@users.noreply.github.com>
2023-08-29 17:53:51 -0400

    added osc routing by column name

--- csv_to_osc.py
+++ csv_to_osc.py
@@ -11,49 +11,50 @@ class Client:
         self.ip = IP
         self.df = pd.read_csv(filepath)
 
-    def send_by_rate(self, ports, rate, columns):
+    def send_by_rate(self, port, rate, columns):
         '''
-            ports: array of ints
+            port: int
             rate: int (number of rows per second)
             columns: array of strings
 
         '''
 
-        clients = {col: SimpleUDPClient(self.ip, port) for col, port in zip(columns, ports)}
+        client = SimpleUDPClient(self.ip, port)
 
         for _, row in self.df.iterrows():
             for col in columns:
-                clients[col].send_message("/", row[col])
+                print(col)
+                client.send_message(f"/{col}", row[col])
             time.sleep(1/rate)
 
 
-    def send_by_time(self, ports, rate, columns, time_col, format):
+    def send_by_time(self, port, rate, columns, time_col, format):
         """
-            ports: array of ints
+            port: int
             rate: int 
-            columsn: array of strings
-            time: string
+            columns: array of strings
+            time_col: string
             format: string
 
         """
         last_time = datetime.datetime.strptime(self.df[time_col].values[0], format)
-        clients = {col: SimpleUDPClient(self.ip, port) for col, port in zip(columns, ports)}
+        client = SimpleUDPClient(self.ip, port)
         for _, row in self.df.iterrows():
             curr_time = datetime.datetime.strptime(row[time_col] + "000", format) # 000 may not be necessary see datetime docs
-            time.sleep(((curr_time - last_time).total_seconds())/rate)
+            time.sleep(((curr_time - last_time).total_seconds())/rate) # figure out what rate means here like mathematicaly
             last_time = curr_time
 
             for col in columns:
-                clients[col].send_message("/", row[col])
+                client.send_message(f"/{col}", row[col])
             
             
-        
+# testing
 
 cl = Client("127.0.0.1", "catalog.csv")
 print(cl.df.columns)
 
 format = "%Y-%m-%d %H:%M:%S.%f"
-# cl.send_by_rate([8888], 3, ["origin_time"])
+# cl.send_by_rate(8888, 3, ["latitude", "longitude"])
 
-cl.send_by_time([8888], 10000, ["latitude"], "origin_time", format)
+cl.send_by_time(8888, 10000, ["latitude"], "origin_time", format)
 
