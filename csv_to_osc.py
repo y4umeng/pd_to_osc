ad39911a5a3cc1c8e5f116ba67e8ba53a093ad21
Yau-Meng Wong <102400085+y4umeng@users.noreply.github.com>
2023-09-23 17:12:33 -0400

    began creating command line interface

--- csv_to_osc.py
+++ csv_to_osc.py
@@ -1,60 +1,80 @@
-from pythonosc.udp_client import SimpleUDPClient
-import pandas as pd
-import time
-import datetime
+import argparse
+import os
+from models import ClientServer
 
-class Client:
-    ip: str
-    df: pd.DataFrame
+"""
+    --filepath
+    --ip (default local)
+    --port
+    --cols (default all)
+    --time-col
+    --format
+    --rate
+    --rate-ip (default to local ot if --ip is set to that)
+    --rate-port
 
-    def __init__ (self, IP, filepath):
-        self.ip = IP
-        self.df = pd.read_csv(filepath)
+"""
 
-    def send_by_rate(self, port, rate, columns):
-        '''
-            port: int
-            rate: int (number of rows per second)
-            columns: array of strings
+parser = argparse.ArgumentParser(
+    description="Process raw registration forms into a standard csv."
+)
 
-        '''
+parser.add_argument(
+    "--filepath", "-fp", type=str, help="File path for CSV", required=True
+)
 
-        client = SimpleUDPClient(self.ip, port)
+parser.add_argument(
+    "--ip", "-ip", type=str, help="IP address to send csv data over.", default="127.0.0.1"
+)
 
-        for _, row in self.df.iterrows():
-            for col in columns:
-                print(col)
-                client.send_message(f"/{col}", row[col])
-            time.sleep(1/rate)
+parser.add_argument(
+    "--port", "-p", type=int, help="Port to send csv data over", required=True
+)
+
+parser.add_argument(
+    "--columns", "--cols", "-c", nargs="+", help="CSV columns to send", required=True
+)
+
+parser.add_argument(
+    "--time", "-t", type=str, help="Column containing time data."
+)
+
+parser.add_argument(
+    "--format", "-f", type=str, help="Datetime format string"
+)
+
+parser.add_argument(
+    "--rate", "--r", type=int, help="Rate at which data is sent. Messages/seconds.", default=5
+)
+
+parser.add_argument(
+    "--rate_ip", "-ri", type=str, help="IP address to send live rate data over.", default="127.0.0.1"
+)
+
+parser.add_argument(
+    "--rate_port", "-rp", type=int, help="Port to send rate data over.", default = None
+)
+
+parser.add_argument(
+    "--infinite", "-inf", action="store_true", help="Infinite loop flag."
+)
 
 
-    def send_by_time(self, port, rate, columns, time_col, format):
-        """
-            port: int
-            rate: int 
-            columns: array of strings
-            time_col: string
-            format: string
 
-        """
-        last_time = datetime.datetime.strptime(self.df[time_col].values[0], format)
-        client = SimpleUDPClient(self.ip, port)
-        for _, row in self.df.iterrows():
-            curr_time = datetime.datetime.strptime(row[time_col] + "000", format) # 000 may not be necessary see datetime docs
-            time.sleep(((curr_time - last_time).total_seconds())/rate) # figure out what rate means here like mathematicaly
-            last_time = curr_time
-
-            for col in columns:
-                client.send_message(f"/{col}", row[col])
-            
-            
-# testing
-
-cl = Client("127.0.0.1", "catalog.csv")
-print(cl.df.columns)
-
-format = "%Y-%m-%d %H:%M:%S.%f"
-# cl.send_by_rate(8888, 3, ["latitude", "longitude"])
-
-cl.send_by_time(8888, 10000, ["latitude"], "origin_time", format)
+def main():
+    args = parser.parse_args()
+    print("Hello!")
+    if not os.path.exists(args.filepath):
+        raise FileNotFoundError(
+            f"Could not find file: {args.filepath}. Please check that you spelled the file name "
+            "correctly and are running the command from the correct directory."
+        )
+    
+    cs = ClientServer(args.ip, args.filepath, )
+    if args.time:
+        if not args.format: 
+            raise ValueError(print("Datetime format must be specified if a time column is provided."))
+        cs.send_by_time(args.port, args.columns, args.rate, args.rate_port, args.time, args.format)
+    else:
+        cs.send_by_rate(args.port, args.columns, args.rate, args.rate_port)
 
